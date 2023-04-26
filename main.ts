6383665eb90a642858e217c1dcc22f7ba316f13b
Shree716 <122548030+Shree716@users.noreply.github.com>
2023-04-26 11:31:14 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,5 +18,31 @@ joystickbit.initJoystickBit()
 radio.setGroup(13)
 basic.showIcon(IconNames.Silly)
 basic.forever(function () {
-	
+    if (action == 0) {
+        forward()
+    }
+    if (action == 2) {
+        reverse()
+    }
+    if (action == 6) {
+        forkUp()
+    }
+    if (action == 7) {
+        forkDown()
+    } else if (action == 7) {
+        wuKong.stopAllMotor()
+    }
+})
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+        radio.sendNumber(1)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
+        radio.sendNumber(2)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.setGroup(7)
+    } else {
+        radio.setGroup(0)
+    }
 })
